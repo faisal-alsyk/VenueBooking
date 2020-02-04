4670bfe3401bef23e09e8fb7071de08dfa94842f
Furqan Mohsin <furqan@stackworx.co>
2020-02-04 16:50:13 +0500

    Added Validation and Updated Priority Bookings

--- app/Validation/venues.js
+++ app/Validation/venues.js
@@ -6,33 +6,27 @@ module.exports = {
         let {name, size, status, venueId} = req.body;
         if ( !name ) {
             errName = "Name for Venue is Required";
-            return res.json({status: "Failed", name: "Name for Venue is Required"});
         }
         else{
             let venueCount = await venueModel.findOne({name: name}).count();
             if ( venueCount > 0 ) {
                 errName = "Name already taken";
-                return res.json({status: "Failed", name: "Name already taken"});
             }
         }
         if ( !venueId ) {
             errVenue = "Venue Id is Required";
-            return res.json({status: "Failed", venueId: "Venue Id is Required"});
         }
         else{
             let venueCount = await venueModel.findOne({venueId: venueId}).count();
             if ( venueCount > 0 ) {
                 errVenue = "Venue ID should be Unique";
-                return res.json({status: "Failed", venueId: "Venue ID should be Unique"});
             }
         }
         if ( size === 0 ){
             errSize = "Size should be greater than 0";
-            return res.json({status: "Failed", size: "Size should be greater than 0."});
         }
         if ( !status ) {
             errStatus = "Please define the status of venue";
-            return res.json({status: "Failed", role: "Please define the status of venue."});
         }
         if(errName || errSize || errStatus || errVenue) {
             let error = {
