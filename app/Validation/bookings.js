4097bc9ce53803be9f4759c9ca1ace37084815b8
Furqan Mohsin <furqan@stackworx.co>
2020-02-10 15:19:56 +0500

    Updated Booking Validation

--- app/Validation/bookings.js
+++ app/Validation/bookings.js
@@ -4,117 +4,95 @@ const userModel = require('../users/model');
 const moment = require('moment');
 
 module.exports = {
-    createBooking: async (req, res, next) =>{
-        let {errTitle, errVenue, errStart, errEnd, errEmail, errPhoneNumber} = "";
-        let {title, venueId,  start, end, email, phoneNumber} = req.body;
-        if ( !title ) {
+    createBooking: async (req, res, next) => {
+        let { errTitle, errVenue, errStart, errEnd, errEmail, errPhoneNumber } = "";
+        let { title, venueId, start, end, email, phoneNumber } = req.body;
+        if (!title) {
             errTitle = "Please provide a booking title";
         }
-        if ( !venueId ) {
+        if (!venueId) {
             errVenue = "Venue should be selected";
         }
         else {
-            let venueCount = await venueModel.findOne({_id: venueId}).count();
-            if ( venueCount < 1 ) {
+            let venueCount = await venueModel.findOne({ _id: venueId }).count();
+            if (venueCount < 1) {
                 errVenue = "Venue does not exist";
             }
         }
-        if ( !start ) {
+        if (!start) {
             errStart = "Booking Start Time is not provided";
         }
-        else {
-            let startTime = moment(start, 'YYYY-MM-DD h:mm:ss', true).isValid();
-            if (!startTime) {
-                errStart = "Format is not Valid";
-            }
-        }
-        if ( !end ) {
+        if (!end) {
             errEnd = "Booking End Time is not provided";
         }
         else {
-            let endTime = moment(end, 'YYYY-MM-DD h:mm:ss', true).isValid();
-            if (!endTime) {
-                errEnd = "Format is not Valid";
+            let validTime = new Date(end) - new Date(start);
+            if (validTime <= 0) {
+                errEnd = "End Time should be greater than Start Time";
             }
         }
-        let validTime = new Date(end) - new Date(start);
-        if ( validTime <= 0 ){
-            errEnd = "End Time should be greater than Start Time";
-        }
-
-        if ( email ) {
-            let userCount = await userModel({email: email}).count();
-            if (userCount > 0){
+        if (email) {
+            let userCount = await userModel({ email: email }).count();
+            if (userCount > 0) {
                 errEmail = "Email already Taken";
             }
         }
 
-        if ( phoneNumber ) {
-            let userCount = await userModel({phoneNumber: phoneNumber}).count();
-            if (userCount > 0){
+        if (phoneNumber) {
+            let userCount = await userModel({ phoneNumber: phoneNumber }).count();
+            if (userCount > 0) {
                 errPhoneNumber = "PhoneNumber Already Taken";
             }
         }
 
-        if ( errTitle || errVenue || errStart || errEnd || errEmail || errPhoneNumber ) {
+        if (errTitle || errVenue || errStart || errEnd || errEmail || errPhoneNumber) {
             let error = {
                 title: errTitle,
                 venueId: errVenue,
-                start: errStart, 
+                start: errStart,
                 end: errEnd,
                 email: errEmail,
                 phoneNumber: errPhoneNumber
             }
-            return res.json({status: "Failed", error: error});
+            return res.json({ status: "Failed", error: error });
         }
         next();
     },
     updateBooking: async (req, res, next) => {
-        let {errTitle, errVenue, errStart, errEnd} = "";
-        let {title, venueId,  start, end} = req.body;
-        if ( !title ) {
+        let { errTitle, errVenue, errStart, errEnd } = "";
+        let { title, venueId, start, end } = req.body;
+        if (!title) {
             errTitle = "Please provide a booking title";
         }
-        if ( !venueId ) {
+        if (!venueId) {
             errVenue = "Venue should be selected";
         }
         else {
-            let venueCount = await venueModel.findOne({_id: venueId}).count();
-            if ( venueCount < 1 ) {
+            let venueCount = await venueModel.findOne({ _id: venueId }).count();
+            if (venueCount < 1) {
                 errVenue = "Venue does not exist";
             }
         }
-        if ( !start ) {
+        if (!start) {
             errStart = "Booking Start Time is not provided";
         }
-        else {
-            let startTime = moment(start, 'YYYY-MM-DD h:mm:ss', true).isValid();
-            if (!startTime) {
-                errStart = "Format is not Valid";
-            }
-        }
-        if ( !end ) {
+        if (!end) {
             errEnd = "Booking End Time is not provided";
         }
         else {
-            let endTime = moment(end, 'YYYY-MM-DD h:mm:ss', true).isValid();
-            if (!endTime) {
-                errEnd = "Format is not Valid";
+            let validTime = new Date(end) - new Date(start);
+            if (validTime <= 0) {
+                errEnd = "End Time should be greater than Start Time";
             }
         }
-        let validTime = new Date(end) - new Date(start);
-        if ( validTime <= 0 ){
-            errEnd = "End Time should be greater than Start Time";
-        }
-
-        if ( errTitle || errVenue || errStart || errEnd ) {
+        if (errTitle || errVenue || errStart || errEnd) {
             let error = {
                 title: errTitle,
                 venueId: errVenue,
-                start: errStart, 
+                start: errStart,
                 end: errEnd
             }
-            return res.json({status: "Failed", error: error});
+            return res.json({ status: "Failed", error: error });
         }
         next();
     }
